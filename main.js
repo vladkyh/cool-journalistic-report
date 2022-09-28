672f5c07a495a987ab011768c3b09e8487706bf1
gay <mraends@gmail.com>
2022-09-28 18:36:24 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+var zapMove = anime ({
+    targets: '#zap',
+    opacity: 0,
+    loop: true,
+    easing: 'linear',
+    duration: 400,
+    endDelay: 150,
+    direction: 'alternate'
+    
+});
+var zapMove = anime ({
+    targets: '#zapp',
+    opacity: 0,
+    loop: true,
+    easing: 'linear',
+    duration: 500,
+    direction: 'alternate',
+    endDelay: 200,
+});
